702128176d5ee52f75c5e8a3d6b082dbd7785b7f
bveinz <velosobruno61@gmail.com>
2024-10-18 11:42:48 -0300

    proyecto de ejemplo para manejar la cache terminado

---
+++ app/messages/layout.js
@@ -0,0 +1,16 @@
+import { getMessages } from "@/lib/messages";
+
+export default function MessagesLayout({ children }) {
+
+  const messages = getMessages();
+  const totalMessages = messages.length
+
+  return (
+    <>
+      <h1>Important Messages</h1>
+      <p>{totalMessages} messages found</p>
+      <hr />
+      {children}
+    </>
+  );
+}
---
+++ app/messages/page.js
@@ -0,0 +1,13 @@
+import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
+
+export default function MessagesPage() {
+
+  const messages = getMessages()
+
+  if (!messages || messages.length === 0) {
+    return <p>No messages found</p>;
+  }
+
+  return <Messages messages={messages} />;
+}
